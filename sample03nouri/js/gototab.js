6099a4af64bf4fcee521b5a5a568582882cd8d01
ningningc <ningningc@cw.com.tw>
2021-06-30 17:41:33 +0800

    sample03nouri

---
+++ sample03nouri/js/gototab.js
@@ -0,0 +1,19 @@
+function goToTab1() {
+ var elmnt = document.getElementById("intro-tab-pane");
+ elmnt.scrollIntoView();
+}
+
+function goToTab2() {
+  var elmnt = document.getElementById("QA-tab-pane");
+  elmnt.scrollIntoView();
+}
+
+function goToTab3() {
+ var elmnt = document.getElementById("news-tab-pane");
+ elmnt.scrollIntoView();
+}
+
+function goToTab4() {
+  var elmnt = document.getElementById("message-tab-pane");
+  elmnt.scrollIntoView();
+ }
